cc88bf2d7da93df5bae60a0a1c091dac9774e58b
Norahxj <Nhaljuhani@gmail.com>
2025-10-14 05:33:42 +0000

    MyRealcommit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,2 @@
 #!/bin/bash
-#include <stdio.h>
-
-/**
- * main - Prints a message using puts
- *
- * Return: 0 (success)
- */
-int main(void)
-{
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
-}
-
+#include <stdio.h> int main(void){puts("Programming is like building a multilingual puzzle");return (0);}

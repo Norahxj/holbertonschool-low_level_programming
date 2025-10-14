ed38c001ff74d429d7bdf76c5d5c6da1502457c2
Norahxj <Nhaljuhani@gmail.com>
2025-10-14 08:50:25 +0000

    MyRealcommit

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,13 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: Prints "Programming is like building a multilingual puzzle"
- *              followed by a new line using the puts function.
+ * Description: Prints "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

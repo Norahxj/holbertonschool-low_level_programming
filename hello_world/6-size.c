4a38a7379f51fa69e2961d44619e3e07e3e57282
Norahxj <Nhaljuhani@gmail.com>
2025-10-14 08:54:49 +0000

    MyRealcommit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,3 +1,15 @@
-#!/bin/bash
-#include <stdio.h>int main(void){printf("with proper grammar, but the outcome is a piece of art,\n");return 0;}
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: prints exactly 'with proper grammar, but the outcome is a piece of art'
+ * followed by a new line using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

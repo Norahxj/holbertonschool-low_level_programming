95b10156d09c83ebc8910a433206fc705aefeed8
Norahxj <Nhaljuhani@gmail.com>
2025-10-14 06:45:46 +0000

    MyRealcommit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints a message using puts
  *
- * Description: Prints "Programming is like building a multilingual puzzle"
- *              followed by a new line using puts function.
- *
- * Return: Always 0 (Success)
+ * Return: 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
 }

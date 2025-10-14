a90c579b1ecb730d598e14bae94f76d2c2715b72
Norahxj <Nhaljuhani@gmail.com>
2025-10-14 07:12:40 +0000

    Myrealcommit

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,3 +1,15 @@
-#!/bin/bash
-#include <stdio.h>int main(void){puts("Programming is like building a multilingual puzzle");return 0;}
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ *              followed by a new line using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}

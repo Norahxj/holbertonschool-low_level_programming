fa3f449d87289cdb8263fad841f54066587e61bf
Norahxj <Nhaljuhani@gmail.com>
2025-10-16 12:19:06 +0000

    fix

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ *
+ * Description: Uses only putchar to print 0 to 9 followed by a new line.
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
 	int n;
 
 	for (n = 0; n < 10; n++)
-	{
-		printf("%d\n", n);
-	}
+		putchar(n + '0');
+	putchar('\n');
 
 	return (0);
 }

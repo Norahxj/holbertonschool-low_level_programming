1a70fe4a17b92a4e3b8490dcc2dbd24409847042
Norahxj <Nhaljuhani@gmail.com>
2025-10-16 09:53:54 +0000

    Myrealcommit

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ *
+ * main 
+ *
+ * Description
+ *
+ * Return: Always
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0, n < 10, n++)
+{
+	printf("%d/n", n);
+}
+	return (0);
+}

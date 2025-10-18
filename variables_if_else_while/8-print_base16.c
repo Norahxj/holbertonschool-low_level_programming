f197c7fabb657e0ae792de6b42a9733bb0ce94cb
Norahxj <Nhaljuhani@gmail.com>
2025-10-18 15:18:32 +0000

    fi

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,13 +10,14 @@
 int main(void)
 {
 	char c;
-	{
-	for (c = '0'; c <= '9'; c++)
-		putchar(0);
-	}
-	for (c = 'a'; c <= 'f'; c++)
-		putchar(0);
 
+
+	for (c = '0'; c <= 'f'; c++)
+{
+		if (c > '9' && c < 'a')
+			continue;
+		putchar(c);
+}
 	putchar('\n');
 	return (0);
 }

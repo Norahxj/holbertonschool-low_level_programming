fd25addf19b02b044ebf9f64aedfe6baeb233395
Norahxj <Nhaljuhani@gmail.com>
2025-10-18 16:59:47 +0000

    new

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Prototype for _putchar (provided by the school) */
+int _putchar(char c);
+
+/* Add your function prototypes here */
+/* Example prototype: */
+void print_alphabet(void);
+
+#endif /* MAIN_H */

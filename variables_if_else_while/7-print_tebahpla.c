8e87d7a62d13621c7570203c4dd23bd37d385343
Norahxj <Nhaljuhani@gmail.com>
2025-10-18 14:23:51 +0000

    try

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase alphabet in reverse
+ *
+ * Description: print all the alphabet
+ *
+ * Return: 0 after success
+ */
+int main(void)
+{
+	char c,
+
+	     for ( c = z; c>= a; c--)
+		     putchar (c);
+	putchar('n\');
+	
+	return (0)'
+}

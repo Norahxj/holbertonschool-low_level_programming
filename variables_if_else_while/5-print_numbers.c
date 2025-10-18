33bb53e867b7fd7cbbe4b6aae02c6853ef93373e
Norahxj <Nhaljuhani@gmail.com>
2025-10-18 13:23:12 +0000

    fix

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - digits from 0 to 10
+ *
+ * Description: print all single digits numbers
+ *
+ * Return: 0 after success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n + '0');
+	putchar('\n')
+
+		return (0);
+}

02730abe647f3c406bc5071c8ee996cc1c8160d0
Norahxj <Nhaljuhani@gmail.com>
2025-10-18 13:41:46 +0000

    new

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main - print all number starting from 0 to 10
+ *
+ * Description: including number from 0 to 10
+ *
+ * Return: 0 after success
+ */
+
+int main(void)
+{
+	int n;
+	
+	for (n + 0; n < 10; n++);
+	putchar(n + '0');
+	putchar ('\n);
+
+	return (0);
+}

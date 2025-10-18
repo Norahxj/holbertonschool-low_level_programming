7b56fe591bc651d6a6f538e59af1f0bb1f990616
Norahxj <Nhaljuhani@gmail.com>
2025-10-18 17:19:37 +0000

    fix

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,11 @@
  *
  * Description: prints letters 'a' to 'z' 10 times, each followed by a new line
  */
-void (print_alphabet_xx10)void
+void print_alphabet_x10(void)
 {
 	int i, c;
 
-	for (i =0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 
 		for (c = 'a'; c <= '2'; c++)

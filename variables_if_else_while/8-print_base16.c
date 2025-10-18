c08b0568f8e851d09c90d18e9d1a2d237c20aae3
Norahxj <Nhaljuhani@gmail.com>
2025-10-18 14:57:11 +0000

    new

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - write base 16 numbers
+ *
+ * Description: creat a program to print all the number in base 16 in lowercase
+ *
+ * Return: 0 after success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar (0);
+
+	for (c = 'a'; c <= 'f'; c++)
+		putchar (0);
+
+	return (0);
+}
+

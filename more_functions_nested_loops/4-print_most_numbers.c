f80070bbb1f57862dbdf3467ccedb84f0cd50515
Norahxj <Nhaljuhani@gmail.com>
2025-10-22 10:59:10 +0000

    fix

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,14 @@
  *
  * Return: (0)
  */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	int n;
 
-	for (n <= 0; n = 0; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		if (n != 2 && n != 4)
 			_putchar(n + '0');
 	}
-	_putchar('\n')
+	_putchar('\n');
 }

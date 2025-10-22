2c4849fce45513c274e06d7ac543e5dd0a77d026
Norahxj <Nhaljuhani@gmail.com>
2025-10-22 11:42:29 +0000

    new

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers from 0 to 14, 10 times
+ * followed by a new line
+ *
+ * Return: (0)
+ */
+void more_numbers(void);
+{
+	int i, j;
+
+	for (i = 0; i < 14; i++)
+	{
+		j = 'a';
+		while (j <= 'z')
+			_putchar(j++);
+		_putchar('\n');
+	}
+}

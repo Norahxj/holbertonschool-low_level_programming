3e8f1f56fe199209d5ca8ad19ea78de722c47abc
Norahxj <Nhaljuhani@gmail.com>
2025-10-22 10:35:31 +0000

    new

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print number from 0 to 9 followed by a new line
+ *
+ * Return: (0)
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+
+	return(0);
+}

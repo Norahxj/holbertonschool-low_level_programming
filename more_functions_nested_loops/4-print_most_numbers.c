d4909f78b9c2b1d9a810f63669c2d02bc6dfe098
Norahxj <Nhaljuhani@gmail.com>
2025-10-22 10:54:16 +0000

    new

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - from 0 to 9 and followed by a new line
+ * exclude 2 and 4
+ *
+ * Return: (0)
+ */
+void print_most_numbers(void);
+{
+	int n;
+
+	for (n <= 0; n = 0; n++)
+	{
+		if (n != 2 && n != 4)
+			_putchar(n + '0');
+	}
+	_putchar('\n')
+}

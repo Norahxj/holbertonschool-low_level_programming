03b50eb01daa3678700ff2888e07638334e83826
Norahx <Nhaljuhani@gmail.com>
2025-10-25 19:03:45 +0000

    new

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * Print_line - printing a straight line
+ *
+ * Description: (n) is the number of times to print _
+ */
+void print_line(int n)
+{
+	int n;
+
+	for (n = 0; n <= 0; n++)
+		_putchar(_'\n');
+}

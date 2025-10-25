cb01dcaf242a03cacb02ea88686511625749c23a
Norahx <Nhaljuhani@gmail.com>
2025-10-25 18:46:20 +0000

    fi

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,14 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 14; i++)
+	for (i = 0; i < 10; i++)
 	{
-		j = 'a';
-		while (j <= 'z')
-			_putchar(j++);
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
 		_putchar('\n');
 	}
 }

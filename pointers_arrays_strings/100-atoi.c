397f51f9bfad79a7c49393bc15fb4665e0a50245
Norahxj <Nhaljuhani@gmail.com>
2025-10-26 08:43:43 +0000

    fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,24 +3,33 @@
  * _atoi - converts a string to an integer
  * @s: pointer to the string to convert
  *
- * Return: the integer value
+ * Return: the converted integer
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, num = 0;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int started = 0;
 
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+
+			if (started && result > (2147483647 - (s[i] - '0')) / 10)
+				return (sign == 1 ? 2147483647 : -2147483648);
+
+			result = result * 10 + (s[i] - '0');
+			started = 1;
+		}
+		else if (started)
+			break;
+
 		i++;
 	}
 
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		num = num * 10 + (s[i] - '0');
-		i++;
-	}
-
-	return (num * sign);
+	return (result * sign);
 }

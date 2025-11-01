9da8dc72455efc343ded56f3ec9ea2259b080455
Norahxj <Nhaljuhani@gmail.com>
2025-11-01 20:46:35 +0000

    New

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -6,4 +6,14 @@
  * @n: bytes
  * Return: dest
  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsighned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}

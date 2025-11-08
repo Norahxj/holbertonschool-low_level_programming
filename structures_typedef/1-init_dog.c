ebc130b356419a2da25be2212d23faf452ca6cf4
Norahxj <Nhaljuhani@gmail.com>
2025-11-08 08:40:11 +0000

    fix

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  * Description: This function assigns values to the fields
  * of a struct dog variable.
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return;

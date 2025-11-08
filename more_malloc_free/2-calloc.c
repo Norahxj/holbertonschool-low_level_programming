15b2dfbedb8b00ba3a9070103f5157de5a7cef9b
Norahxj <Nhaljuhani@gmail.com>
2025-11-08 02:26:45 +0000

    fix

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,19 +10,20 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
-	unsigned int total_size;
+	char *ptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	total_size = nmemb * size;
-
-	ptr = malloc(total_size);
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
 
-	memset(ptr, 0, total_size);
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		ptr[i] = 0;
+	}
 
 	return (ptr);
 }

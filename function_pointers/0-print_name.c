94518d04f8c42440904da3c8498d535a5671a670
Norahxj <Nhaljuhani@gmail.com>
2025-11-10 09:17:27 +0000

    fix

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 /**
- * Print_name - Prints a name using a given function
+ * print_name - Prints a name using a given function
  * @name: Name to print
  * @f: Pointer to a function that takes a char * and returns void
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;

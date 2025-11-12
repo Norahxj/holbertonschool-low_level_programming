e3d89483edb9ace117a050ec9a72ddfa8e748347
Norahxj <Nhaljuhani@gmail.com>
2025-11-12 08:58:59 +0000

    fix

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,15 +1,15 @@
-#ifndef Calc_H
+#ifndef CALC_H
 #define CALC_H
 /**
- * struct op - to Struct op
+ * struct op - to associate an operator with a function
  * @op: The operator
- * @f: The function
+ * @f: The function that corresponds to the operator
  */
 typedef struct op
 {
 	char *op;
 
-	int (*f)(int a; int b);
+	int (*f)(int a, int b);
 } op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);

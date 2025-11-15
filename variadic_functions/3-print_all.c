53191aa3f3287875de62e884c28353e7386ea06b
Norahxj <Nhaljuhani@gmail.com>
2025-11-15 21:21:42 +0000

    new

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - prints anything based on format string
+ * @format: list of types of arguments passed
+ * c: char, i: integer, f: float, s: char *
+ * ...: variable arguments to be printed
+ *
+ * Description: If string is NULL, prints "(nil)".
+ * Any other char in format is ignored.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	char *str;
+	char *sep = "";
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == 'c')
+			printf("%s%c", sep, va_arg(args, int));
+		if (format[i] == 'i')
+			printf("%s%d", sep, va_arg(args, int));
+		if (format[i] == 'f')
+			printf("%s%f", sep, va_arg(args, double));
+		if (format[i] == 's')
+		{
+			str = va_arg(args, char *);
+			if (!str)
+				str = "(nil)";
+			printf("%s%s", sep, str);
+		}
+		if (format[i] == 'c' || format[i] == 'i' ||)
+		    (format[i] == 'f' || format[i] == 's')
+			sep = ", ";
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
+}

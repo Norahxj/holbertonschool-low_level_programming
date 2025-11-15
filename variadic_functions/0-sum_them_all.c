a70250a019ee1918f4e209b67e35ef19e2b8600f
Norahxj <Nhaljuhani@gmail.com>
2025-11-15 20:43:06 +0000

    new

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -6,3 +6,24 @@
  *
  * Return: sum of all parameters, or 0 if n == 0
  */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+
+	va_list(args);
+	va_list(args, n);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+
+	va_end(args);
+
+	return (sum);
+}

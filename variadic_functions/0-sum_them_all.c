f97435b7bbeec6017324f06a65ea90a09527b228
Norahxj <Nhaljuhani@gmail.com>
2025-11-15 20:34:56 +0000

    Addition

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,8 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - returns the sum of all parameters
+ * @n: number of arguments
+ *
+ * Return: sum of all parameters, or 0 if n == 0
+ */

518e65d811f3112098db842deb865249f5b4d424
Norahxj <Nhaljuhani@gmail.com>
2025-11-27 07:58:36 +0000

    added the needed code fot task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+        size_t count = 0;
+
+        while (h != NULL)
+        {
+                h = h->next;
+                        count++;
+        }
+        return (count);
+}

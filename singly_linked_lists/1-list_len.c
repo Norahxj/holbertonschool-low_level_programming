7b0d97c11dc3f64bbc2407efba5ca98437497f10
Norahxj <Nhaljuhani@gmail.com>
2025-11-30 07:22:16 +0000

    Fixed betty style documentation

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,19 +2,19 @@
 #include <stdio.h>
 
 /**
- * print_list - prints all the elements of a list_t list
+ * list_len - returns the number of elements in a linked list_t list
  * @h: pointer to the list
  *
  * Return: number of nodes
  */
 size_t list_len(const list_t *h)
 {
-        size_t count = 0;
+	size_t count = 0;
 
-        while (h != NULL)
-        {
-                h = h->next;
-                        count++;
-        }
-        return (count);
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
